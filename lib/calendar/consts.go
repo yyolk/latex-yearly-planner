43f9e4eafa19a07dfa6d0e0ebc04647e2ce71234
Vitaliy Kudryk <kudryk.v@protonmail.com>
2022-03-16 22:06:23 +0200

    some consts 🚧

--- lib/calendar/consts.go
+++ lib/calendar/consts.go
@@ -1,6 +1,13 @@
 package calendar
 
 const (
+	FirstQuarter = iota
+	SecondQuarter
+	ThirdQuarter
+	FourthQuarter
+)
+
+const (
 	// usualWeeksInMonth defines a number of how many weeks usually there are in a month.
 	usualWeeksInMonth = 5
 
