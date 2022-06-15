38cf5bfee8957060e73919c7580d86bc27194cb2
Vitaliy Kudryk <contact@bgnfu7re.me>
2022-06-15 22:33:52 +0300

    💩

--- app2/planners/mos/mos_monthly_stuff.go
+++ app2/planners/mos/mos_monthly_stuff.go
@@ -1,31 +1,8 @@
 package mos
 
 import (
-	"fmt"
-
-	"github.com/kudrykv/latex-yearly-planner/app2/texsnippets"
 	"github.com/kudrykv/latex-yearly-planner/lib/calendar"
-	"github.com/kudrykv/latex-yearly-planner/lib/texcalendar"
 )
-
-type mosMonthlyHeader struct {
-	year calendar.Year
-}
-
-func (m mosMonthlyHeader) Build() ([]string, error) {
-	texYear := texcalendar.NewYear(m.year)
-
-	header, err := texsnippets.Build(texsnippets.MOSHeader, map[string]string{
-		"MarginNotes": texYear.Months() + `\qquad{}` + texYear.Quarters(),
-		"Header":      "hello world header",
-	})
-
-	if err != nil {
-		return nil, fmt.Errorf("build header: %w", err)
-	}
-
-	return []string{header}, nil
-}
 
 type mosMonthlyContents struct {
 	month calendar.Month
--- app2/planners/mos/mos_quarterly_stuff.go
+++ app2/planners/mos/mos_quarterly_stuff.go
@@ -1,31 +1,9 @@
 package mos
 
 import (
-	"fmt"
-
-	"github.com/kudrykv/latex-yearly-planner/app2/texsnippets"
 	"github.com/kudrykv/latex-yearly-planner/lib/calendar"
 	"github.com/kudrykv/latex-yearly-planner/lib/texcalendar"
 )
-
-type mosQuarterlyHeader struct {
-	year calendar.Year
-}
-
-func (r mosQuarterlyHeader) Build() ([]string, error) {
-	texYear := texcalendar.NewYear(r.year)
-
-	built, err := texsnippets.Build(texsnippets.MOSHeader, map[string]string{
-		"MarginNotes": texYear.Months() + `\qquad{}` + texYear.Quarters(),
-		"Header":      "hello world header",
-	})
-
-	if err != nil {
-		return nil, fmt.Errorf("texsnippets build: %w", err)
-	}
-
-	return []string{built}, nil
-}
 
 type mosQuarterlyContents struct {
 	quarter calendar.Quarter
