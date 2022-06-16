224c7577fabfc2c69aaef155a4a89502584f2c37
Vitaliy Kudryk <contact@bgnfu7re.me>
2022-06-16 22:04:48 +0300

    💩

---
+++ app2/pages/buffer.go
@@ -0,0 +1,27 @@
+package pages
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type Buffer struct {
+	*bytes.Buffer
+}
+
+func NewBuffer() *Buffer {
+	return &Buffer{&bytes.Buffer{}}
+}
+
+func (r *Buffer) WriteBlocks(blocks ...Block) error {
+	compiledPages, err := NewPage(blocks...).Build()
+	if err != nil {
+		return fmt.Errorf("build pages from blocks: %w", err)
+	}
+
+	for _, page := range compiledPages {
+		_, _ = r.WriteString(page)
+	}
+
+	return nil
+}
