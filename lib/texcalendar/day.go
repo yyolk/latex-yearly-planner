c5ee63f8330b13ef749a5c7353f68c474f147110
Vitaliy Kudryk <contact@bgnfu7re.me>
2022-06-24 19:42:15 +0300

    🚧 shareable params in texcalendar

--- lib/texcalendar/day.go
+++ lib/texcalendar/day.go
@@ -9,11 +9,18 @@ import (
 type Days []Day
 
 type Day struct {
-	Day calendar.Day
+	Day        calendar.Day
+	parameters Parameters
 }
 
-func NewDay(day calendar.Day) Day {
-	return Day{Day: day}
+func NewDay(day calendar.Day, options ...ApplyToParameters) Day {
+	parameters := Parameters{}
+
+	for _, option := range options {
+		option(&parameters)
+	}
+
+	return Day{Day: day, parameters: parameters}
 }
 
 func (d Day) NameAndDate() string {
