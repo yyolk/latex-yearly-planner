dea301cf344813bb367b6937d522fdd2b078de1b
Vitaliy Kudryk <contact@bgnfu7re.me>
2022-07-10 17:57:57 +0300

    💩

--- app2/planners/mos/contents_daily.go
+++ app2/planners/mos/contents_daily.go
@@ -3,6 +3,7 @@ package mos
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/kudrykv/latex-yearly-planner/app2/planners/common"
 	"github.com/kudrykv/latex-yearly-planner/app2/tex/ref"
@@ -29,39 +30,66 @@ func (r dailyContents) Build() ([]string, error) {
 }
 
 func (r dailyContents) prioritiesAndNotesColumn() string {
+	return fmt.Sprintf(
+		prioritiesAndNotesColumnFormat,
+		strings.Join(r.newTodos(), "\n"),
+		ref.NewNote("More", r.day.Ref()).Build(),
+		ref.NewReflect("Reflect", r.day.Ref()).Build(),
+	)
+}
+
+func (r dailyContents) newTodos() []string {
 	var priorities []string
 
-	for i := 0; i < 8; i++ {
+	for i := 0; i < r.ui.TodosNumber; i++ {
 		priorities = append(priorities, r.height()+`$\square$\myLineGray`)
 	}
-
-	moreNotes := ref.NewNote("More", r.day.Ref()).Build()
-	dailyReflect := ref.NewReflect("Reflect", r.day.Ref()).Build()
-
-	return `\begin{minipage}[t]{\dimexpr2\myLengthThreeColumnWidth+\myLengthThreeColumnsSeparatorWidth}
-\myUnderline{Top Priorities}
-` + strings.Join(priorities, "\n") + `
-\vskip7mm\myUnderline{Notes | ` + moreNotes + ` \hfill{}` + dailyReflect + `}
-\vspace{5mm}\hspace{.5mm}\vbox to 0mm{\myDotGrid{30}{19}}
-\end{minipage}`
+	return priorities
 }
 
 func (r dailyContents) scheduleColumn() string {
-	var hours []string
+	hoursSchedule := r.hoursSchedule()
 
-	for i := 8; i <= 21; i++ {
-		strHour := fmt.Sprintf("%0d", i)
-		hours = append(hours, r.height()+strHour+`\myLineLightGray
-\vskip5mm\myLineGray`)
+	return fmt.Sprintf(scheduleColumnFormat, hoursSchedule, r.optionalCalendar())
+}
+
+func (r dailyContents) hoursSchedule() string {
+	var schedule []string
+
+	for i := r.ui.FromScheduleHour; i <= r.ui.ToScheduleHour; i++ {
+		formattedHour := time.Date(0, 0, 0, i, 0, 0, 0, time.Local).Format(r.ui.HourFormat)
+		schedule = append(schedule, r.scheduleHour(formattedHour))
 	}
 
-	return `\begin{minipage}[t]{\myLengthThreeColumnWidth}
-\myUnderline{Schedule\textcolor{white}{g}}
-` + strings.Join(hours, "\n") + `
-\vskip5mm` + r.day.CalendarMonth().Selected(r.day).LittleCalendar() + `
-\end{minipage}`
+	return strings.Join(schedule, "\n")
+}
+
+func (r dailyContents) scheduleHour(strHour string) string {
+	return r.height() + strHour + `\myLineLightGray
+\vskip5mm\myLineGray`
 }
 
 func (r dailyContents) height() string {
 	return `\parbox{0pt}{\vskip5mm}`
 }
+
+func (r dailyContents) optionalCalendar() string {
+	if !r.ui.EnableCalendarOnDailyPages {
+		return ""
+	}
+
+	return r.day.CalendarMonth().Selected(r.day).LittleCalendar()
+}
+
+const prioritiesAndNotesColumnFormat = `\begin{minipage}[t]{\dimexpr2\myLengthThreeColumnWidth+\myLengthThreeColumnsSeparatorWidth}
+\myUnderline{Top Priorities}
+%s
+\vskip7mm\myUnderline{Notes | %s \hfill{}%s}
+\vspace{5mm}\hspace{.5mm}\vbox to 0mm{\myDotGrid{30}{19}}
+\end{minipage}`
+
+const scheduleColumnFormat = `\begin{minipage}[t]{\myLengthThreeColumnWidth}
+\myUnderline{Schedule\textcolor{white}{g}}
+%s
+%s
+\end{minipage}`
--- app2/planners/mos/parameters.go
+++ app2/planners/mos/parameters.go
@@ -22,7 +22,12 @@ type ui struct {
 
 	LittleCalArrayStretch string
 
-	LargeCalHeaderHeight string
+	LargeCalHeaderHeight       string
+	TodosNumber                int
+	FromScheduleHour           int
+	ToScheduleHour             int
+	HourFormat                 string
+	EnableCalendarOnDailyPages bool
 }
 
 func newUI(layout common.Layout) (ui, error) {
@@ -37,6 +42,13 @@ func newUI(layout common.Layout) (ui, error) {
 			LittleCalArrayStretch: "1.6",
 
 			LargeCalHeaderHeight: "5mm",
+
+			TodosNumber: 8,
+
+			FromScheduleHour:           8,
+			ToScheduleHour:             21,
+			HourFormat:                 "15",
+			EnableCalendarOnDailyPages: true,
 		}, nil
 	default:
 		return ui{}, fmt.Errorf("%s: %w", layout.Name, common.UnknownDeviceErr)
