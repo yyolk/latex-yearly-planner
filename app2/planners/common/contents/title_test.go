dde9fcd3872070d99c385d0f18679d72f2b85a57
Vitaliy Kudryk <contact@bgnfu7re.me>
2022-08-06 12:29:21 +0300

    test title ✅
    gotta start somewhere

---
+++ app2/planners/common/contents/title_test.go
@@ -0,0 +1,25 @@
+package contents_test
+
+import (
+	"testing"
+
+	"github.com/kudrykv/latex-yearly-planner/app2/planners/common/contents"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestTitle(t *testing.T) {
+	t.Parallel()
+
+	Convey("should build a title", t, func() {
+		title := contents.NewTitle("title")
+
+		build, err := title.Build()
+		So(err, ShouldBeNil)
+		So(build, ShouldHaveLength, 1)
+		So(build[0], ShouldEqual, expectedTitle)
+	})
+}
+
+const expectedTitle = `\hspace{0pt}\vfil
+\hfill\resizebox{.7\linewidth}{!}{title}%
+\pagebreak`
