7fa23d62e60d53148a7876cfe4869aa75a8d3af3
Vitaliy Kudryk <contact@bgnfu7re.me>
2022-09-02 20:50:30 +0300

    🚧

--- app3/sections/index.go
+++ app3/sections/index.go
@@ -3,16 +3,21 @@ package sections
 import (
 	"errors"
 	"fmt"
+	"strings"
+
+	"github.com/kudrykv/latex-yearly-planner/app3/types"
 )
 
 type IndexParameters struct {
 	ItemsPerPage int
 	Pages        int
+	LineHeight   types.Millimeters
 }
 
 var (
 	ErrNonPositiveItemsPerPage = errors.New("negative index per page")
 	ErrNonPositivePages        = errors.New("negative pages")
+	ErrNonPositiveLineHeight   = errors.New("negative line height")
 )
 
 func (r IndexParameters) Test() error {
@@ -22,6 +27,10 @@ func (r IndexParameters) Test() error {
 
 	if r.Pages <= 0 {
 		return ErrNonPositivePages
+	}
+
+	if r.LineHeight <= 0 {
+		return ErrNonPositiveLineHeight
 	}
 
 	return nil
@@ -44,9 +53,21 @@ func NewIndex(parameters IndexParameters) (Index, error) {
 func (r Index) Build() ([]string, error) {
 	pages := make([]string, 0, r.parameters.Pages)
 
-	for i := 0; i < r.parameters.Pages; i++ {
-		pages = append(pages, fmt.Sprintf("index %d", i))
+	for i := 1; i <= r.parameters.Pages; i++ {
+		items := make([]string, 0, r.parameters.ItemsPerPage)
+
+		for j := 1; j <= r.parameters.ItemsPerPage; j++ {
+			itemNumber := (i-1)*r.parameters.ItemsPerPage + j
+
+			items = append(items, fmt.Sprintf(`%d & \parbox{0pt}{\vskip%s} \\ \hline`, itemNumber, r.parameters.LineHeight))
+		}
+
+		pages = append(pages, fmt.Sprintf(indexTemplate, strings.Join(items, "\n")))
 	}
 
 	return pages, nil
 }
+
+const indexTemplate = `{\arrayrulecolor{gray}\begin{tabularx}{\linewidth}{l|l}
+%s
+\end{tabularx}}`
