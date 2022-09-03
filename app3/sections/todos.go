b91cba1fec6f82b89160b2583dc40e68a97245b2
Vitaliy Kudryk <contact@bgnfu7re.me>
2022-09-03 18:56:34 +0300

    🚧

--- app3/sections/todos.go
+++ app3/sections/todos.go
@@ -1,21 +1,33 @@
 package sections
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/kudrykv/latex-yearly-planner/app3/components"
+)
 
 type TodosParameters struct {
 	IndexParameters IndexParameters
+	TodosParameters components.TodosParameters
 }
 
 type Todos struct {
 	parameters TodosParameters
 	page       int
 	index      Index
+	todos      components.Todos
 }
 
 func NewTodos(index Index, parameters TodosParameters) (Todos, error) {
+	todos, err := components.NewTodos(parameters.TodosParameters)
+	if err != nil {
+		return Todos{}, fmt.Errorf("new todos: %w", err)
+	}
+
 	return Todos{
 		parameters: parameters,
 		index:      index,
+		todos:      todos,
 	}, nil
 }
 
@@ -34,5 +46,5 @@ func (r Todos) Reference() string {
 }
 
 func (r Todos) Build() ([]string, error) {
-	return []string{"hello todo"}, nil
+	return []string{r.todos.Build()}, nil
 }
