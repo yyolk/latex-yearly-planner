692db56d734e34ea867d34d59d7d730f370bbf5d
Vitaliy Kudryk <contact@bgnfu7re.me>
2022-09-05 20:14:25 +0300

    🚧

--- app3/sections/weekly.go
+++ app3/sections/weekly.go
@@ -3,21 +3,42 @@ package sections
 import (
 	"fmt"
 
+	"github.com/kudrykv/latex-yearly-planner/app3/components"
+	"github.com/kudrykv/latex-yearly-planner/app3/types"
 	"github.com/kudrykv/latex-yearly-planner/lib/calendar"
 )
 
 type WeeklyParameters struct {
+	OneColumnWidth      types.Millimeters
+	TwoColumnWidth      types.Millimeters
+	Separator           types.Millimeters
+	OneColumnParameters components.NotesParameters
+	TwoColumnParameters components.NotesParameters
 }
 
 type Weekly struct {
-	parameters WeeklyParameters
-	week       calendar.Week
+	parameters  WeeklyParameters
+	week        calendar.Week
+	oneColNotes components.Notes
+	twoColNotes components.Notes
 }
 
 func NewWeekly(week calendar.Week, parameters WeeklyParameters) (Weekly, error) {
+	oneColNotes, err := components.NewNotes(parameters.OneColumnParameters)
+	if err != nil {
+		return Weekly{}, fmt.Errorf("new notes: %w", err)
+	}
+
+	twoColNotes, err := components.NewNotes(parameters.TwoColumnParameters)
+	if err != nil {
+		return Weekly{}, fmt.Errorf("new notes: %w", err)
+	}
+
 	return Weekly{
-		parameters: parameters,
-		week:       week,
+		parameters:  parameters,
+		week:        week,
+		oneColNotes: oneColNotes,
+		twoColNotes: twoColNotes,
 	}, nil
 }
 
@@ -36,7 +57,50 @@ func (w Weekly) Title() string {
 }
 
 func (w Weekly) Build() ([]string, error) {
-	return []string{
-		"hello week",
-	}, nil
+	return []string{fmt.Sprintf(
+		weeklyTemplate,
+
+		w.parameters.OneColumnWidth,
+		w.oneColNotes.Build(),
+		w.parameters.Separator,
+		w.parameters.OneColumnWidth,
+		w.oneColNotes.Build(),
+		w.parameters.Separator,
+		w.parameters.OneColumnWidth,
+		w.oneColNotes.Build(),
+
+		w.parameters.OneColumnWidth,
+		w.oneColNotes.Build(),
+		w.parameters.Separator,
+		w.parameters.OneColumnWidth,
+		w.oneColNotes.Build(),
+		w.parameters.Separator,
+		w.parameters.OneColumnWidth,
+		w.oneColNotes.Build(),
+
+		w.parameters.OneColumnWidth,
+		w.oneColNotes.Build(),
+		w.parameters.Separator,
+		w.parameters.TwoColumnWidth,
+		w.twoColNotes.Build(),
+	)}, nil
 }
+
+const weeklyTemplate = `\parbox{%s}{\myUnderline{hello world}\par{}%s}%%
+\hspace{%s}%%
+\parbox{%s}{\myUnderline{hello world}\par{}%s}%%
+\hspace{%s}%%
+\parbox{%s}{\myUnderline{hello world}\par{}%s}%%
+\vfill{}
+
+\parbox{%s}{\myUnderline{hello world}\par{}%s}%%
+\hspace{%s}%%
+\parbox{%s}{\myUnderline{hello world}\par{}%s}%%
+\hspace{%s}%%
+\parbox{%s}{\myUnderline{hello world}\par{}%s}%%
+\vfill{}
+
+\parbox{%s}{\myUnderline{hello world}\par{}%s}%%
+\hspace{%s}%%
+\parbox{%s}{\myUnderline{Notes}\par{}%s}
+`
