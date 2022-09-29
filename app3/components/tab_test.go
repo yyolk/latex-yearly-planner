9fea15df8111c2750b9c40e30065e0be9cee4237
Vitaliy Kudryk <contact@bgnfu7re.me>
2022-09-29 14:48:55 +0300

    test tab ✅
    and test shows how `target` name makes absolutely no sense here

---
+++ app3/components/tab_test.go
@@ -0,0 +1,34 @@
+package components_test
+
+import (
+	"testing"
+
+	"github.com/kudrykv/latex-yearly-planner/app3/components"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTab_String(t *testing.T) {
+	tests := map[string]struct {
+		tab  components.Tab
+		want string
+	}{
+		"name": {
+			tab:  components.Tab{Text: "hello"},
+			want: `\hyperlink{hello}{hello}`,
+		},
+		"reference": {
+			tab:  components.Tab{Text: "hello", Reference: "world"},
+			want: `\hyperlink{world}{hello}`,
+		},
+		"target": {
+			tab:  components.Tab{Text: "hello", Target: true},
+			want: `\cellcolor{black}{\textcolor{white}{hello}}`,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, test.want, test.tab.String())
+		})
+	}
+}
