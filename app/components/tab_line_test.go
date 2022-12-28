8b69c9fe1fa1dd2f840d2988ad52936908251a82
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2022-12-28 11:51:26 +0200

    test tab line ✅

---
+++ app/components/tab_line_test.go
@@ -0,0 +1,27 @@
+package components_test
+
+import (
+	"testing"
+
+	"github.com/kudrykv/latex-yearly-planner/app/components"
+	. "github.com/kudrykv/latex-yearly-planner/app/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTabLine_Build(t *testing.T) {
+	t.Parallel()
+
+	tabs := components.Tabs{
+		{Text: "one", Reference: "ref-one", Target: true},
+		{Text: "two", Reference: "ref-two", Target: false},
+	}
+	parameters := components.TabLineParameters{
+		VerticalSpacing:     5,
+		SpaceBetweenColumns: 3,
+	}
+
+	tabLine := components.NewTabLine(tabs, parameters)
+
+	build := tabLine.Build()
+	assert.Equal(t, Fixture("tab_line"), build)
+}
